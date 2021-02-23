11ac76ddeeb40271aed74e5f27e798d1777a9c65
OutOfCache <outofcache@tutanota.com>
2021-02-24 00:34:36 +0100

    First 16 Instructions
    00-0xF

--- internal/cpu.go
+++ internal/cpu.go
@@ -111,7 +111,7 @@ func (cpu *CPU) cpu06() int { // LD B, u8
 }
 
 func (cpu *CPU) cpu07() int { // RLCA
-	if ((cpu.reg.A & 0x80) >> 7) == 1 {
+	if (cpu.reg.A & 0x80) == 0x80 {
 		cpu.flg.C = true
 	} else {
 		cpu.flg.C = false
@@ -130,5 +130,87 @@ func (cpu *CPU) cpu08() int { // LD (u16),SP
 	Write(address+1, byte((cpu.reg.SP&0xFF00)>>8))
 
 	cpu.reg.PC += 3
+	return 0
+}
+
+func (cpu *CPU) cpu09() int { // ADD HL, BC
+	cpu.flg.N = false
+	sum := int(cpu.reg.L) + int(cpu.reg.C)
+	cpu.reg.L = byte(sum & 0xFF)
+	cpu.flg.H = (byte(sum>>8)+(cpu.reg.H&0xf)+(cpu.reg.B&0xf))&0x10 == 0x10
+	sum = (sum >> 8) + int(cpu.reg.H) + int(cpu.reg.B)
+	cpu.reg.H = byte(sum & 0xFF)
+	cpu.flg.C = sum > 0xFF
+
+	cpu.reg.PC++
+
+	return 0
+}
+
+func (cpu *CPU) cpu0A() int { // LD A, (BC)
+	address := uint16(cpu.reg.B)<<8 + uint16(cpu.reg.C)
+	cpu.reg.A = Read(address)
+
+	cpu.reg.PC++
+	return 0
+}
+
+func (cpu *CPU) cpu0B() int { // DEC BC
+	if cpu.reg.C == 0 && cpu.reg.B > 0 {
+		cpu.reg.C = 0xFF
+		cpu.reg.B--
+	} else {
+		cpu.reg.C--
+	}
+
+	cpu.reg.PC++
+	return 0
+}
+
+func (cpu *CPU) cpu0C() int { // INC C
+	cpu.flg.N = false
+	cpu.reg.C++
+	if cpu.reg.C == 0 {
+		cpu.flg.Z = true
+	}
+	if cpu.reg.C == 0x10 {
+		cpu.flg.H = true
+	}
+
+	cpu.reg.PC++
+	return 0
+}
+
+func (cpu *CPU) cpu0D() int { // DEC C
+	cpu.flg.N = true
+	cpu.reg.C--
+	if cpu.reg.C == 0 {
+		cpu.flg.Z = true
+	}
+	if cpu.reg.C == 0xF {
+		cpu.flg.H = true
+	}
+
+	cpu.reg.PC++
+	return 0
+}
+
+func (cpu *CPU) cpu0E() int {
+	cpu.reg.PC++
+	cpu.reg.C = Read(cpu.reg.PC)
+
+	cpu.reg.PC++
+	return 0
+}
+
+func (cpu *CPU) cpu0F() int {
+	cpu.flg.Z = false
+	cpu.flg.N = false
+	cpu.flg.H = false
+	cpu.flg.C = cpu.reg.A&0x01 == 0x01
+	cpu.reg.A >>= 1
+	cpu.reg.A += FlagToBit(cpu.flg.C) << 7
+
+	cpu.reg.PC++
 	return 0
 }
--- internal/cpu_test.go
+++ internal/cpu_test.go
@@ -143,3 +143,106 @@ func TestCpu08(t *testing.T) {
 	}
 
 }
+
+func TestCpu09(t *testing.T) {
+	var tests = []struct {
+		pc         uint16
+		h          byte
+		l          byte
+		b          byte
+		c          byte
+		expectedH  byte
+		expectedL  byte
+		expectedHF bool
+		expectedC  bool
+		expectedPC uint16
+	}{
+		{0x1234, 0x5E, 0x38, 0x7D, 0x89, 0xDB, 0xC1, true, false, 0x1235},
+		{0x63F8, 0x82, 0x35, 0x6A, 0x12, 0xEC, 0x47, false, false, 0x63F9},
+		{0x63F8, 0x82, 0x35, 0x8A, 0x12, 0x0C, 0x47, false, true, 0x63F9},
+	}
+
+	for _, test := range tests {
+		cpu.reg.PC = test.pc
+		cpu.reg.H = test.h
+		cpu.reg.L = test.l
+		cpu.reg.B = test.b
+		cpu.reg.C = test.c
+
+		cpu.cpu09()
+		if cpu.reg.H != test.expectedH {
+			t.Errorf("H: %x, expected: %x", cpu.reg.H, test.expectedH)
+		}
+		if cpu.reg.L != test.expectedL {
+			t.Errorf("L: %x, expected: %x", cpu.reg.L, test.expectedL)
+		}
+		if cpu.flg.H != test.expectedHF {
+			t.Errorf("H Flag: %t, expected %t", cpu.flg.H, test.expectedHF)
+		}
+		if cpu.flg.C != test.expectedC {
+			t.Errorf("C Flag: %t, expected %t", cpu.flg.C, test.expectedC)
+		}
+		if cpu.reg.PC != test.expectedPC {
+			t.Errorf("Current PC: %x, expected: %x", cpu.reg.PC, test.expectedPC)
+		}
+	}
+
+}
+
+func TestCpu0A(t *testing.T) {
+	var tests = []struct {
+		pc         uint16
+		B          byte
+		C          byte
+		address    uint16
+		val        byte
+		expectedPC uint16
+		A          byte
+	}{
+		{0x9432, 0x13, 0x7F, 0x137F, 0x8E, 0x9433, 0x35},
+		{0x2F3C, 0x30, 0x49, 0x3049, 0x3C, 0x2F3D, 0xF3},
+	}
+
+	for _, test := range tests {
+		cpu.reg.PC = test.pc
+		cpu.reg.B = test.B
+		cpu.reg.C = test.C
+		Write(test.address, test.val)
+		cpu.cpu0A()
+		if cpu.reg.PC != test.expectedPC {
+			t.Errorf("Current PC %x; expected: %x", cpu.reg.PC, test.expectedPC)
+		}
+		if cpu.reg.A != test.val {
+			t.Errorf("Current A: %x; expected: %x", cpu.reg.A, test.val)
+		}
+	}
+}
+
+func TestCpu0F(t *testing.T) {
+	var tests = []struct {
+		pc         uint16
+		a          byte
+		carry      bool
+		expectedA  byte
+		expectedPC uint16
+	}{
+		{0x1234, 0x01, true, 0x80, 0x1235},
+		{0x63F8, 0x32, false, 0x19, 0x63F9},
+	}
+
+	for _, test := range tests {
+		cpu.reg.PC = test.pc
+		cpu.reg.A = test.a
+		cpu.cpu0F()
+		if cpu.flg.C != test.carry {
+			t.Errorf("Carry: %t, expected: %t", cpu.flg.C, test.carry)
+		}
+		if cpu.reg.A != test.expectedA {
+			t.Errorf("Current A: %x, expected %x", cpu.reg.A, test.expectedA)
+		}
+		if cpu.reg.PC != test.expectedPC {
+			t.Errorf("Current PC: %x, expected: %x", cpu.reg.PC, test.expectedPC)
+		}
+	}
+
+}
