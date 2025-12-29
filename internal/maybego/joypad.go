89c8aeb5e03da76907d82cdf0ff05fd44486a5b2
OutOfCache <jessica.del76@gmail.com>
2025-12-29 22:01:38 +0100

    [Joypad] Track all the buttons internally
    On HW, the Gameboy uses multiplexers to figure out the sources of the
    button presses.
    
    In an emulator, we track both direction and action buttons and only
    write the enabled ones into JOYP.

--- internal/maybego/joypad.go
+++ internal/maybego/joypad.go
@@ -1,11 +1,99 @@
 package maybego
 
-const JOYP uint16 = 0xFF00
+import "fmt"
 
 type Joypad struct {
 	prev_joypad byte
+	directions  byte
+	buttons     byte
 }
 
+type Direction byte
+type Button byte
+
+const JOYP uint16 = 0xFF00
+
+const (
+	DirectionRight Direction = iota
+	DirectionLeft
+	DirectionUp
+	DirectionDown
+)
+
+const (
+	ButtonA Button = iota
+	ButtonB
+	ButtonSelect
+	ButtonStart
+)
+
 func NewJoypad() *Joypad {
-	return &Joypad{prev_joypad: Read(JOYP)}
+	Write(0xFF00, 0x3F)
+	return &Joypad{prev_joypad: Read(JOYP), directions: 0xF, buttons: 0xF}
 }
+
+func (joy *Joypad) updateControls() {
+	new_joypad := Read(JOYP)
+	// The buttons are only read on a write to JOYP
+	// If there was no change, there is no need
+	// changed := new_joypad != joy.prev_joypad
+
+	// if !changed {
+	// 	return
+	// }
+
+	directions_enabled := (new_joypad & 0x30) == 0x20
+	buttons_enabled := (new_joypad & 0x30) == 0x10
+
+	if !directions_enabled && !buttons_enabled {
+		joy.prev_joypad = new_joypad | 0xF // all buttons disabled
+		Write(JOYP, joy.prev_joypad)
+		return
+	}
+
+	if directions_enabled {
+		new_joypad |= joy.directions
+		// joy.buttons = 0xF
+		// fmt.Println("reset buttons")
+	} else {
+		new_joypad |= joy.buttons
+		// joy.directions = 0xF
+		// fmt.Println("reset directions")
+	}
+
+	bitmask := byte(0x1)
+	for i := range 4 {
+		if new_joypad&bitmask == 0 /*&& joy.prev_joypad&bitmask == 0*/ {
+			RequestInterrupt(4)
+		}
+		bitmask <<= 1
+	}
+
+	joy.prev_joypad = new_joypad
+	Write(JOYP, new_joypad)
+}
+
+func (joy *Joypad) setButton(b Button) {
+	mask := byte(^(1 << b))
+	joy.buttons &= mask
+	fmt.Printf("Buttons: %X\n", joy.buttons)
+}
+
+func (joy *Joypad) resetButton(b Button) {
+	mask := byte((1 << b))
+	joy.buttons |= mask
+	fmt.Printf("Buttons: %X\n", joy.buttons)
+}
+
+func (joy *Joypad) setDirection(d Direction) {
+	mask := byte(^(1 << d))
+	joy.directions &= mask
+	fmt.Printf("Directions: %X\n", joy.directions)
+}
+
+func (joy *Joypad) resetDirection(d Direction) {
+	mask := byte((1 << d))
+	joy.directions |= mask
+	fmt.Printf("Directions: %X\n", joy.directions)
+}
+
