6d1da53dec03d86197a9d9ed20b42af829de4987
OutOfCache <jessica.del76@gmail.com>
2026-04-03 08:11:44 +0200

    [PPU][OAM] DMA

--- internal/maybego/memory.go
+++ internal/maybego/memory.go
@@ -6,7 +6,10 @@ import (
 )
 
 const (
-	BANK uint16 = 0xFF50 // Whether the boot rom has finished loading
+	BANK     uint16 = 0xFF50 // Whether the boot rom has finished loading
+	DMA      uint16 = 0xFF46 // OAM DMA
+	OAMSTART uint16 = 0xFE00
+	OAMEND   uint16 = 0xFE9F
 )
 
 var Memory [65536]byte
@@ -36,9 +39,21 @@ func Read(adr uint16) byte {
 	return Memory[adr]
 }
 
+func writeOamDma(src uint16) {
+	// TODO: 160 cycles delay
+	src_slice := Memory[(src << 8) : (src<<8)|0x9F]
+	copy(Memory[0xFE00:0xFE9F], src_slice)
+}
+
 func Write(adr uint16, val byte) {
 	if adr < 0x8000 && Memory[adr] != 0 {
 		return
+	}
+
+	// OAM DMA: written val is the source address << 8
+	// Dest: 0xFE00-0xFE9F
+	if adr == DMA {
+		writeOamDma(uint16(val))
 	}
 	Memory[adr] = val
 }
